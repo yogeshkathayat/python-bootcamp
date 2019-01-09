475dd31f58fdd770c39fc9ccbd5f9ac7889b5313
yogesh <kathayat.yogesh@gmail.com>
2019-01-09 15:02:26 +0400

    hello world

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+# import modules used here -- sys is a very standard one
+import sys;
+
+# Gather our code in a main() function
+def main():
+
+    if len(sys.argv)>1:
+        print 'Hello There', sys.argv[1]     
+        # Command line args are in sys.argv[1], sys.argv[2] ...
+        # sys.argv[0] is the script name itself and can be ignored
+    else:
+        print 'Hello There Guest'
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == "__main__":
+    main()
+
